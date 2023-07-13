97fce8f219149040f37dee53b424599dab2fc3e0
GamaSatria <mgama6969@gmail.com>
2023-07-13 18:58:47 +0700

    project uas

---
+++ resources/views/admins/admin.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.admin.admin')
+@section('content')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Anggota Kelompok</h1>
+            <ul>
+                <li>Zahra Qonita</li>
+                <li>Sir Gama</li>
+                <li>Athaya Saskia Gotik</li>
+                <li>Prince Salman Alfarisi</li>
+            </ul>
+        </div>
+    </div>
+@endsection
